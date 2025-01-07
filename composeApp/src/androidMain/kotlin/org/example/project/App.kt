c8862aec86a8d21b8e17d41ee3fe6fe1ee738bee
ovinidev <vinicius.celestino@pagar.me>
2025-01-07 11:17:17 -0300

    create project

--- composeApp/src/androidMain/kotlin/org/example/project/App.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/App.kt
@@ -2,11 +2,13 @@ package org.example.project
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,20 +19,19 @@ import androidx.compose.runtime.Composable
 @Composable
 fun App() {
     var buttonColor by remember { mutableStateOf(Color.Red) }
+    var showAnotherButton by remember { mutableStateOf(false) }
+    var count by remember { mutableIntStateOf(0) }
 
     fun toggleColor(color: Color) {
         buttonColor = if (color == Color.Red) Color.Blue else Color.Red
     }
 
     var randomText by remember { mutableStateOf("Loading...") }
-
-    LaunchedEffect(Unit) {
+    LaunchedEffect(count) {
         delay(2000)
         randomText = generateRandomText()
     }
-
     MaterialTheme {
-        var count by remember { mutableIntStateOf(0) }
         Column(
             Modifier
                 .fillMaxWidth()
@@ -38,13 +39,17 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
+            TextField(value = randomText, onValueChange = { randomText = it })
             Button(
                 onClick = {
-                    count += 1; toggleColor(buttonColor);randomText = generateRandomText()
+                    count += 1
+                    toggleColor(buttonColor)
+                    randomText = generateRandomText()
+                    showAnotherButton = !showAnotherButton
                 },
                 colors = androidx.compose.material.ButtonDefaults.buttonColors(backgroundColor = buttonColor)
             ) {
-                Text(text = "Click Me 22", color = Color.White)
+                Text(text = "Click Me", color = Color.White)
             }
             Text(
                 text = count.toString(),
@@ -53,6 +58,11 @@ fun App() {
                 text = randomText,
                 style = MaterialTheme.typography.body1
             )
+            if (showAnotherButton) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text("Another Button")
+                }
+            }
         }
     }
 }
