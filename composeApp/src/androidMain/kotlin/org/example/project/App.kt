fb363a8bfbb4b2c4d27f851e9c8d8fcfb0db5880
ovinidev <vinicius.celestino@pagar.me>
2025-01-08 15:25:30 -0300

    feat: create todo list

--- composeApp/src/androidMain/kotlin/org/example/project/App.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/App.kt
@@ -1,10 +1,18 @@
 package org.example.project
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -12,26 +20,56 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import kotlinx.coroutines.delay
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import java.util.UUID
+
+data class TodoItem(val id: String, val value: String)
 
 @Composable
 fun App() {
-    var buttonColor by remember { mutableStateOf(Color.Red) }
-    var showAnotherButton by remember { mutableStateOf(false) }
-    var count by remember { mutableIntStateOf(0) }
+    var todo by remember { mutableStateOf("") }
 
-    fun toggleColor(color: Color) {
-        buttonColor = if (color == Color.Red) Color.Blue else Color.Red
+    var todoList by remember { mutableStateOf(listOf(TodoItem(id = "", value = ""))) }
+
+    fun addTodoItem(id: String, value: String) {
+        if (value.isEmpty()) return
+        todoList = todoList + TodoItem(id, value)
+        todo = ""
     }
 
-    var randomText by remember { mutableStateOf("Loading...") }
-    LaunchedEffect(count) {
-        delay(2000)
-        randomText = generateRandomText()
+    fun removeTodoItem(id: String) {
+        todoList = todoList.filter { it.id != id }
     }
+
     MaterialTheme {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+            ) {
+                TextField(
+                    value = todo,
+                    onValueChange = { todo = it },
+                    label = { Text("Enter Text") },
+                    modifier = Modifier.width(200.dp),
+                    singleLine = true
+                )
+                Button(onClick = {
+                    addTodoItem(id = UUID.randomUUID().toString(), value = todo)
+                }) {
+                    Text(
+                        text = "Create task",
+                    )
+                }
+            }
+        }
+
         Column(
             Modifier
                 .fillMaxWidth()
@@ -39,35 +77,41 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            TextField(value = randomText, onValueChange = { randomText = it })
-            Button(
-                onClick = {
-                    count += 1
-                    toggleColor(buttonColor)
-                    randomText = generateRandomText()
-                    showAnotherButton = !showAnotherButton
-                },
-                colors = androidx.compose.material.ButtonDefaults.buttonColors(backgroundColor = buttonColor)
+            TodoListScreen(
+                todoList,
+                removeItem = { id -> removeTodoItem(id) })
+        }
+    }
+
+}
+
+@Composable
+fun TodoListScreen(todoList: List<TodoItem>, removeItem: (String) -> Unit = {}) {
+    LazyColumn {
+        items(todoList) { task ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .background(MaterialTheme.colors.background),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
             ) {
-                Text(text = "Click Here", color = Color.White)
-            }
-            Text(
-                text = count.toString(),
-            )
-            Text(
-                text = randomText,
-                style = MaterialTheme.typography.body1
-            )
-            if (showAnotherButton) {
-                Button(onClick = { /*TODO*/ }) {
-                    Text("Another Button")
+                Text(
+                    text = task.value,
+                    style = MaterialTheme.typography.body1,
+                    color = MaterialTheme.colors.primary
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Button(
+                    onClick = { removeItem(task.id) },
+                ) {
+                    Text(
+                        text = "Remove",
+                        color = MaterialTheme.colors.secondary
+                    )
                 }
             }
         }
     }
-}
-
-fun generateRandomText(): String {
-    val words = listOf("Compose", "Kotlin", "Jetpack", "Android", "Code", "Design", "UI")
-    return words.random()
 }
